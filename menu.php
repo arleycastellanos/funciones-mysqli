c17a28598743efbe40e05296aaa1b40032415f04
arleycastellanos <arley1132133@gmail.com>
2017-04-06 20:00:46 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,18 @@
+<!--
+    Autor: dubier perez
+    Este es o debería ser el primer archivo del aplicativo. Cuando se accede por el index.php una vez instalado el 
+    aplicativo, todo se desplegará desde aquí.
+-->
+
+<html>
+    <head>
+        <title>Este es el eplaicativo.</title>
+    </head>
+
+    <body>
+    
+        Desde aqu&iacute; se deber&iacute;a empezar a construir el aplicativo.
+
+    </body>
+
+</html>
